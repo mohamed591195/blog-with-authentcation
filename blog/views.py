0d1e28d71a95c6a0f40f8f4e97bf6c41a0417816
mohammad <mohamedsamir591195@gmail.com>
2019-02-25 01:17:10 +0200

    The File is prepared for uploading

---
+++ blog/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+# Create your views here.
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+
+    }
+
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/delete_confirm.html'
+    success_url = '/blog/'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+
+    return render(request, 'blog/about.html', {'title': 'about', 'dic1': {'type': 'englis', 'price': 27, 'date': 2000}})
